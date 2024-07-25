532c50413b305aa6a3bc9dd6b60c74c53c4c25b4
Amols231 <shelarabtt@gmail.com>
2024-07-26 01:46:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def delete_all_entries():
     return redirect(url_for('view'))
 
 if __name__ == '__main__':
-    app.run()#(debug=False,host='0.0.0.0')
+    app.run()#(debug=False,host='0.0.0.0',port=8000)
